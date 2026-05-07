6699207ccbab381888fe3fe5130f4933ed5e6247
Abhi <576abhishek.2020@gmail.com>
2026-05-07 17:23:59 +0530

    new

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,4 +1,3 @@
-import API_BASE_URL from './apiConfig.js';
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
 
 export default API_BASE_URL;
